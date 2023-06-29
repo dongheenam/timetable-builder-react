909a3e0a12891a0c62c82a2029846e57dbe97fdd
Donghee Nam <57467269+dongheenam@users.noreply.github.com>
2023-06-29 19:58:08 +1000

    Add react router links

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,11 @@ function AppLayout() {
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />} />
-      <Route index element={<Home />} />
-      <Route path="edit" element={<Edit />} />
-      <Route path="view" element={<View />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route path="edit" element={<Edit />} />
+        <Route path="view" element={<View />} />
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,12 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Header.module.css';
+
+const navItems = [
+  { href: '/', label: 'Home' },
+  { href: '/edit', label: 'Edit' },
+  { href: '/view', label: 'View' },
+];
 
 export default function Header() {
   return (
@@ -8,7 +16,13 @@ export default function Header() {
         <span>v2</span>
       </div>
       <nav className={styles['nav-block']}>
-        <ul className={styles['nav-list']}></ul>
+        <ul className={styles['nav-list']}>
+          {navItems.map(({ href, label }) => (
+            <li className={styles['nav-item']} key={href}>
+              <Link to={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
